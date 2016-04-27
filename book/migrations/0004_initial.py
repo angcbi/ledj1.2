aa0334a7949801b39c8572f5c14791c7f2366d7c
angcbi <angcbi@126.com>
2016-04-27 21:34:19 +0800

    2

---
+++ book/migrations/0004_initial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Book'
+        db.create_table('book_book', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('publication_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('book', ['Book'])
+
+        # Adding model 'Contact'
+        db.create_table('book_contact', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('subject', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('message', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal('book', ['Contact'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Book'
+        db.delete_table('book_book')
+
+        # Deleting model 'Contact'
+        db.delete_table('book_contact')
+
+
+    models = {
+        'book.book': {
+            'Meta': {'object_name': 'Book'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'publication_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'book.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['book']
