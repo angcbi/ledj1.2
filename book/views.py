c8642f2ad403f6d06b315d5f5784c3e45d7ab386
angcbi <angcbi@126.com>
2016-04-27 12:10:12 +0800

    23

--- book/views.py
+++ book/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import render_to_response
 from django.views.generic.simple import direct_to_template
 from django.http import Http404
 
-from book.models import Book
+from .models import Book, Contact
+
 
 def index(request):
     return render_to_response('book/index.html', {'name': 'django'})
@@ -32,3 +33,17 @@ def search(request):
             return render_to_response('search.html', {'books': books, 'q':q})
     print errors
     return render_to_response('search_form.html',{'errors': errors})
+
+# urlconf 传入字典,获取查询model和template名字
+def book_view(request, template_name, model):
+    m_list = model.objects.all()
+    # template_name = 'book/{}_list.html'.format(model.__name__.lower())
+    return render_to_response(template_name, {'m_list':m_list} )
+
+
+def mydate(request, mouth, day):
+    return HttpResponse('2016-{}-{}'.format(mouth, day))
+
+
+def page(request, num=1):
+    return HttpResponse(num)
--- urls.py
+++ urls.py
@@ -2,14 +2,14 @@
 from django.conf.urls.defaults import *
 from mysite1 import views
 from django.conf import settings # from mysite1 imort settings
-# from django.views.generic.simple import  direct_to_template
-# from book.views import about_pages
+from book import  models as book_models
 
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+# 视图，视图字符串，urlconf字典，url分组，url命名分组，视图函数默认参数
 urlpatterns = patterns('',
     # Example:
     # (r'^mysite1/', include('mysite1.foo.urls')),
@@ -28,7 +28,9 @@ urlpatterns = patterns('',
     # 命名组和非命名组是不能同时存在于同一个URLconf,有命名组，会忽略非命名组
     (r'^time/plus/(?P<offset>\d{1,2})/$', views.hours_head),
     (r'^testloop/$', views.testloop),
-    (r'^contact/$', 'contact.views.contact'),
+    (r'^contact_me/$', 'contact.views.contact'),
+    (r'^mydate/(?P<mouth>\d{1,2})/((?P<day>\d{1,2}))/$', 'book.views.mydate' ),
+    (r'^mydate/birthday/$', 'book.views.mydate', {'mouth': '4', 'day': '2'}),
 )
 
 if settings.DEBUG:
@@ -36,7 +38,16 @@ if settings.DEBUG:
         (r'^debug/$', 'views.debug'),
     )
 
+
 urlpatterns += patterns('book.views',
     (r'^search/$', 'search'),
+    # 视图函数只关心获取到参数，不关心参数是url捕获的或者是关键字传值来的
+    # 如果URLconf捕捉到的一个命名组变量和一个额外URLconf参数包含的变量同名时，额外URLconf参数的值会被使用
+    (r'^book/$', 'book_view', {'template_name': 'book_list.html', 'model':book_models.Book}),
+    (r'^contact/$', 'book_view', {'template_name': 'contact_list.html', 'model':book_models.Contact}),
+
+    # 缺省视图函数参数
+    (r'^blog/$', 'page'),
+    (r'^blog/page(?P<num>\d+)/$', 'page'),
 )
 
